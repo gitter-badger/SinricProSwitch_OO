a50d63777527a3e18268b5bd1d80598c42318d5d
Boris Jaeger <sivar2311@googlemail.com>
2021-03-21 20:37:56 +0100

    Changed button to be handled by interrupt

--- src/MySwitch.h
+++ src/MySwitch.h
@@ -1,44 +1,50 @@
 #ifndef _MYSWITCH_H_
 #define _MYSWITCH_H_
 
-#include <SinricProSwitch.h>
+#include <SinricProSwitch.h>                            // this device is based on SinricProSwitch...so include it
+#include <FunctionalInterrupt.h>                        // provide std::bind to bind member functions to interrupt routines (ISR)
 
 class MySwitch : public SinricProSwitch {               // create a new class "MySwitch" which inherits from SinricProSwitch
   public:
-    MySwitch(const String &deviceId, int switch_pin, int button_pin);
+    MySwitch(const String &deviceId, int led_pin, int button_pin);
     virtual bool onPowerState(bool &newState) override; // inherited and overloaded onPowerState (replaces the callback function)
-    void checkButton();                                 // new member function to handle button press (user defined function)
+
   private:
-    int switch_pin;                                     // pin where the output is connected to
+    int led_pin;                                        // pin where the output is connected to
     int button_pin;                                     // pin where the push-button is connected to
     bool powerState;                                    // variable to keep track of the powerState
-    unsigned long lastMillis;                           // used for button debouncing in checkButton()
+    unsigned long lastMillis;                           // used for button debouncing in handleButtonISR()
+    
+    void handleButtonISR();                             // user defined (ISR) function to handle button press
 };
 
-MySwitch::MySwitch(const String &deviceId, int switch_pin, int button_pin)
+MySwitch::MySwitch(const String &deviceId, int led_pin, int button_pin)
  : SinricProSwitch(deviceId)                            // initialize SinricProSwitch 
- , switch_pin(switch_pin)                               // initialize switch_pin
+ , led_pin(led_pin)                                     // initialize switch_pin
  , button_pin(button_pin) {                             // initialize button_pin
-  pinMode(switch_pin, OUTPUT);                          // set pinMode for switch_pin to OUTPUT
+  pinMode(led_pin, OUTPUT);                             // set pinMode for switch_pin to OUTPUT
   pinMode(button_pin, INPUT_PULLUP);                    // set pinMode for button_pin to INPUT_PULLUP (reading is inverted!! HIGH=released, LOW=pressed)
+  attachInterrupt(                                      // attach interrupt
+    button_pin,                                         //   pin wich triggers the interrupt
+    std::bind(&MySwitch::handleButtonISR, this),        //   bind this `handleButtonISR` to the interrupt
+    FALLING                                             //   when signal is FALLING EDGE
+  );  
 }
 
-bool MySwitch::onPowerState(bool &newState) {           // same as callback-function
-  powerState = newState;                                // update local powerState variable
-  digitalWrite(switch_pin, newState);                   // set the pin HIGH or LOW (depending on newState)
+bool MySwitch::onPowerState(bool &newState) {           // same as callback-function (except deviceId <- not necessary anylonger because "we are" the device!)
+  powerState = newState;                                // update powerState variable
+  digitalWrite(led_pin, newState);                      // set the pin HIGH or LOW (depending to newState)
   return true;                                          // return true -> request was properly handled
 }
 
-void MySwitch::checkButton() {                          // this function handles the button press
-  if (digitalRead(button_pin)) return;                  // button pin is HIGH (not pressed)... do nothing
-
-  unsigned long currentMillis = millis();               // }
-  if (currentMillis - lastMillis <= 250) return;        //  } debounce button
-  lastMillis = currentMillis;                           // }
-
+void MySwitch::handleButtonISR() {                      // this (ISR) function handles the button press
+  unsigned long currentMillis = millis();               // .
+  if (currentMillis - lastMillis <= 250) return;        //  . debounce button
+  lastMillis = currentMillis;                           // .
+ 
   bool newState = !powerState;                          // newState is the invers of powerState
-  onPowerState(newState);                               // use onPowerState function to switch the pin to newState
-  sendPowerStateEvent(newState);                        // send Event to let SinricPro know about the change
+  onPowerState(newState);                               // call onPowerState with newState to do the real work (digitalWrite..)
+  sendPowerStateEvent(newState);                        // send event to SinricPro Server with new state
 }
 
 #endif
--- src/credentials-example.h
+++ src/credentials-example.h
@@ -1,11 +1,10 @@
 // fill in your credentials and rename the file to "credentials.h"
 
-#define SSID        "SinricWiFi"
-#define PASS        "SinricWiFi"
+#define SSID        ""
+#define PASS        ""
 
-#define APP_KEY     "efa6b074-e7a6-4914-9f32-47e6f089ebd6"
-#define APP_SECRET  "78a4e0bc-9a09-4f68-8f55-02ff5a7660e9-effbd4f1-01aa-4fb8-af2e-02dae71e8914"
+#define APP_KEY     ""
+#define APP_SECRET  ""
 
-#define DEVICE_ID1  "6056ccae850907071cfc80c0"
-#define DEVICE_ID2  "6056ccc36547d7070e7e8b93"
-
+#define DEVICE_ID1  ""
+#define DEVICE_ID2  ""
